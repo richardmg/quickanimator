1b25036753524be4e06bd0553ff3d460d9ddce42
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2013-05-19 15:11:17 +0200

    add z to data

--- timelinedata.js
+++ timelinedata.js
@@ -4,15 +4,15 @@
 var sprites = [
     [
         // walk (time: 0):
-        { time: 0, x: 0, y: 0, rotation: 0, scale: 1, opacity: 1 },
-        { time: 1, x: 200, y: 0, rotation: 0, scale: 1, opacity: 1 },
-        { time: 2, x: 100, y: 0, rotation: 0, scale: 1, opacity: 1 }
+        { time: 0, x: 0, y: 0, z: 0, rotation: 0, scale: 1, opacity: 1 },
+        { time: 1, x: 200, y: 0, z: 0, rotation: 0, scale: 1, opacity: 1 },
+        { time: 2, x: 100, y: 0, z: 0, rotation: 0, scale: 1, opacity: 1 }
     ],[
         // walk (time: 0):
-        { time: 0, x: 200, y: 0, rotation: 0, scale: 1, opacity: 1 },
-        { time: 1, x: 200, y: 200, rotation: 0, scale: 1, opacity: 1 },
-        { time: 2, x: 200, y: 100, rotation: 0, scale: 1, opacity: 1 },
-        { time: 4, x: 200, y: 100, rotation: 180, scale: 1.5, opacity: 0.5, after:
+        { time: 0, x: 200, y: 0, z: 0, rotation: 0, scale: 1, opacity: 1 },
+        { time: 1, x: 200, y: 200, z: 0, rotation: 0, scale: 1, opacity: 1 },
+        { time: 2, x: 200, y: 100, z: 0, rotation: 0, scale: 1, opacity: 1 },
+        { time: 4, x: 200, y: 100, z: 0, rotation: 180, scale: 1.5, opacity: 0.5, after:
             function(sprite) {
                 sprite.stage.setTime(0, true);
             }
